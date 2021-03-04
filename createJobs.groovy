ad457f7e2c27a4eb154c09b6872968a2a4260104
T,Ravi Shanker Reddy <career4reddy@gmail.com>
2021-03-04 10:40:44 +0530

    Jenkinsfile Commit

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,14 @@
-pipelineJob('pipelineJob') {
+pipelineJob('app-service-job') {
     definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/ravisreddy/app.git'
+                    }
+                    branch 'master'
+                }
+            }
         }
     }
 }
